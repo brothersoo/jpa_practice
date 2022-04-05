2abbebb65edbf2351af07c932f2cdd0f435783d3
brothersoo <53594958+brothersoo@users.noreply.github.com>
2022-04-05 16:09:21 +0900

    MODIFY: enable jpa auditing for createAt updateAt

--- src/main/java/com/brothersoo/jpa_practice/JpaPracticeApplication.java
+++ src/main/java/com/brothersoo/jpa_practice/JpaPracticeApplication.java
@@ -2,8 +2,10 @@ package com.brothersoo.jpa_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaPracticeApplication {
 
 	public static void main(String[] args) {
